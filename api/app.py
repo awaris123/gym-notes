aecdcd3f9510dae87e0dcc8787b6df1219e37c64
Aakef Waris <awaris@hawk.iit.edu>
2019-06-25 21:08:21 -0500

    signup endpoint stores user acc

--- api/app.py
+++ api/app.py
@@ -5,15 +5,12 @@ import mysql.connector
 
 app = Flask(__name__)
 
-
-users = []
-
 mydb = mysql.connector.connect(host="gym-notes.cj6ntdmjz9qf.us-east-1.rds.amazonaws.com",
                                user="gym_log_db_1234" ,
                                passwd="V7qEz8kaqSs4zBN",
                                database="gym_log_db_1234")
 
-cursor = mydb.cursor()
+cursor = mydb.cursor(buffered=True)
 
 cursor.execute("SHOW databases")
 
@@ -24,33 +21,49 @@ def index():
                    weight = 225,
                    date = "Dec-23-1999")
 
+
 # allows user to create accounts
 @app.route('/notes/api/v1.0/sign-up', methods=['POST'])
 def signup():
 
-    name = request.json['username']
+    fname = request.json['firstname']
+    lname = request.json['lastname']
+    user = request.json['username']
     email = request.json['email']
     password = request.json['password']
     confirm = request.json['confirm-pw']
-    if len(users) == 0:
-        id = 0
-    else:
-         id = users[-1].id + 1
+    member = User(user,email,password, fname, lname)
 
-    member = User(name,email,password,id)
-
-    if not name or not email or not password or not confirm:
+    if not fname or not lname or not user or not email or not password or not confirm:
         abort(400) # missing args
 
     if password != confirm:
         abort(400) # password does not match confirm
 
-    for ppl in users:
-        if member == ppl:
-            abort(404) # account credentials not available
-    users.append(member)
+    cursor.execute("SELECT username, email FROM User")
+    results = cursor.fetchall()
 
-    return member.info()
+    issue = ""
+    for res in results:
+        print(res)
+        if res[0] == member.username:
+            issue += "That username already in use\n"
+        if res[1] == member.email:
+            issue += "That email is already associated with another account\n"
+
+    if issue:
+        return issue
+    else:
+        ADD_USER = ("INSERT INTO User "
+                   "(username, email, hashed_pw, firstname, lastname, user_id) "
+                   "VALUES (%s, %s, %s, %s, %s, %s)")
+
+
+        cursor.execute(ADD_USER,member.info())
+        mydb.commit()
+
+
+        return member.json()
 
 # authenticates users and logs them in
 @app.route('/notes/api/v1.0/login', methods = ['GET'])
--- api/models/user.py
+++ api/models/user.py
@@ -3,31 +3,27 @@ from passlib.apps import custom_app_context as pwd
 
 class User:
 
-    def __init__ (self,username="", email="", pw = "", id=0):
+    def __init__ (self,username = "", email = "", pw = "", first_name = "", last_name = "",id=0):
         self.username = username
         self.email = email
         self.hashed_pw = pwd.encrypt(pw)
+        self.first_name = first_name
+        self.last_name = last_name
         self.user_id = id
+
 
     def __eq__(self, other):
         if self.username == other.username or self.email == other.email:
             return True
         else:
             return False
-    def verify_pw(self, password):
-        return pwd.verify(password, self.hashed_pw)
 
-    def set_hash(self, hash):
-        self.hashed_pw = hash
 
     def json(self):
         print(self.hashed_pw)
         return jsonify(username = self.username,
-                       email = self.email,
-                       pw_hash = self.hashed_pw,
-                       id = self.user_id)
+                       email = self.email)
 
     def info(self):
-                return jsonify(username = self.username,
-                               email = self.email,
-                               id = self.user_id)
+
+        return (self.username, self.email, self.hashed_pw, self.first_name, self.last_name,self.user_id)
