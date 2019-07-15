823966890b085e18d0053a8a15ec83f37ac2be0c
Aakef Waris <awaris@hawk.iit.edu>
2019-07-14 20:05:47 -0500

    Start up is now sign in with create account link

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:gym_notes/login.dart';
-import 'register.dart';
+import 'package:gym_notes/start_up.dart';
 
 void main() => runApp(MyApp());
 
@@ -17,47 +16,10 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text("Gym Notes"),
         ),
-          body: Center(child: StartUp(), 
+          body: Center(child: Login(), 
         )
       ),
     );
   }
 }
 
-class StartUp extends StatelessWidget{
-
-  @override
-  Widget build (BuildContext context){
-    return Center(
-      child:Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          const SizedBox(height: 30),
-          OutlineButton(
-            shape:RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-            color: Colors.white,
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => Registration()),
-                );
-            },
-            child: const Text('Sign Up', style: TextStyle(fontSize: 20)),
-          ),
-          const SizedBox(height:30),
-          OutlineButton(
-            shape:RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-            color: Colors.white,
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => Entrance()),
-                );
-            },
-            child: const Text('Login', style: TextStyle(fontSize:20))
-            ),
-        ],
-      )
-    );
-  }
-}
